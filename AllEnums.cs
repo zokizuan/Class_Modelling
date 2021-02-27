f6cbce464945b2403d687c565878997772a69016
zokizuan <zoki.zuan@yahoo.com>
2021-02-27 13:46:40 +0530

    vsmd

---
+++ AllEnums.cs
@@ -0,0 +1,59 @@
+enum ProcessorType
+{   
+    Snapdragon,
+    Mediatek,
+    Exynos,
+    Kirin
+}
+
+enum DisplayType
+{
+    LCD,
+    OLED,
+    AMOLED,
+    SuperAMOLED,
+    IPS
+}
+
+enum RamSize
+{
+    FourGB,
+    SixGB,
+    EightGB,
+    TwelveGB
+}
+
+enum CameraMP
+{
+    Twelve,
+    Sixteen,
+    FortyEight,
+    SixtyFour,
+    OneHundredOne
+}
+
+enum DiskSize
+{
+    SixtyFour,
+    OneTwentyEight,
+    TwoFiftySix,
+    FiveTwelve
+}
+
+enum BrandList
+{
+    Apple,
+    Samsung,
+    Google,
+    Huawei,
+    OnePlus,
+    Xiaomi,
+    LG,
+    Oppo,
+    Vivo,
+    Nokia,
+    Realme,
+    ASus,
+    Motorola,
+    OtherBrands
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace excercise
+{
+    class Smartphone
+    {
+        public BrandList Brand;
+        public ProcessorType Processor;
+        public DisplayType Display;
+        public DiskSize Disk;
+        public RamSize Ram;
+        public CameraMP Camera;
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           MakeASmartphone();
+        }
+
+        static void MakeASmartphone(){
+
+            Smartphone Model = new Smartphone();
+            Console.WriteLine("Select a Brand");
+            for (int i = 0; i < Enum.GetNames(typeof(BrandList)).Length; i++)
+            {
+                Console.WriteLine($"{i}. {Enum.GetNames(typeof(BrandList))[i]}");
+            }
+            Console.ReadKey();
+            // Console.WriteLine($"{}");
+        }
+    }
+}
